97d717faeaa06461eae508a5e167bc9b2430b3fa
Guilherme Monteiro <dkglima@gmail.com>
2024-01-11 16:06:54 -0300

    updates to work with frontend

--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,16 @@ Rails.application.routes.draw do
     namespace :v1 do
       get '/clients', to: 'clients#index'
       get '/clients/:id', to: 'clients#show'
-      get '/client/name', to: 'clients#find_name'
-      post '/clients', to: 'clients#create'
-      put '/client/:id', to: 'clients#update'
+      post '/client/name', to: 'clients#find_name'
+      post '/client/create', to: 'clients#create'
+      patch '/client/:id', to: 'clients#update'
       delete '/client/:id', to: 'clients#destroy'
 
       get '/cities', to: 'cities#index'
       get '/cities/:id', to: 'cities#show'
       get 'city/name', to: 'cities#find_name'
       get 'city/state', to: 'cities#find_state'
-      post '/cities', to: 'cities#create'
+      post '/city/create', to: 'cities#create'
     end
   end
 end
